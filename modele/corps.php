544e807dfe0211f3b0fd34555b4b5c2d4511edb2
franck <17na408@esisalama.org>
2020-04-06 02:19:47 +0200

    resolution conflit 2

--- modele/corps.php
+++ modele/corps.php
@@ -16,11 +16,9 @@
             </p>
           </div>
           <div class="card-action">
-<<<<<<< HEAD
+
             <a class="waves-effect waves-light btn-small" style="background-color: red;">plus</a>
-=======
-            <a class="waves-effect waves-light btn-small" style="background-color: violet;">hi</a>
->>>>>>> inoconnu
+
           </div>
       </div>
     </div>
