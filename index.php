a53c100458eccad13d0344948042a3c6f4268e0c
franck <17na408@esisalama.org>
2020-07-05 12:11:24 +0200

    s

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Design</title>
+    
     <link rel="stylesheet" href="materialize/css/materialize.css">
     <link rel="stylesheet" href="css/iconfont/material-icons.css">
     <link rel="stylesheet" href="swiper/package/css/swiper.min.css">
