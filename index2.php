f278e6fad5620a586ee4473700571a6f2523c81e
franck <17na408@esisalama.org>
2020-07-12 20:58:14 +0200

    js

--- index2.php
+++ index2.php
@@ -2,6 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <link  href="cropperjs/dist/cropper.css" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <title>DoCropper</title>
     <style>
         img{
@@ -11,14 +12,46 @@
     </style>
 </head>
 <body>
-    
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/IMG-20160327-WA0011.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/pdj-08-fev-site.jpg__770x433_q85_crop_subsampling-2_upscale.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/agences.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
     <div>
         <img src="images/IMG_20190721_174414.jpg" alt="" id="image">
     </div>
-
+    
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="cropperjs/dist/cropper.js"></script>
     <script type="module" src="js/main1.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="jquery/src/jquery.js"></script>
+
+   
 </body>
 </html>
 
