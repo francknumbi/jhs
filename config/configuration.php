bae5025aab4e42517429930971fc628b0706b804
franck <17na408@esisalama.org>
2020-11-23 11:55:34 +0200

    configuration connection BD postgres

--- config/configuration.php
+++ config/configuration.php
@@ -1,11 +1,17 @@
 <?
     //definition des differents parametres de la connexion
-    define("SERVEUR","localhost");
-    define("DB","jhs_db");
+    /*define("SERVEUR","localhost");
+    define("DB","jhsdb");
     define("LOG","root");
     define("PWD","");
     define("CONNECTION", "mysql:host=".SERVEUR.";dbname=".DB.";"."charset=utf8");
-
+    */
+    define("SERVEUR","ec2-54-247-169-129.eu-west-1.compute.amazonaws.com");
+    define("DB","da7u2htsam4e4o");
+    define("LOG","bopfflrvqyucxn");
+    define("PWD","9ece4a5bba66ee304151683121998bf3ae4b42aab3d3da3b9f62495ee9e4ed2f");
+    define("PORT","5432");
+    define("CONNECTION", "pgsql:host=".SERVEUR.";port=".PORT.";dbname=".DB);
     class Connexion{
         private $connection;
         public function getConnexion(){
